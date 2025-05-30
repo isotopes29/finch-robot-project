bc27f62d28f1c06708b5261a8876f4969f7b9b3d
will <bcannon0001@mymail.lausd.net>
2025-05-30 15:32:23 -0700

    legs

---
+++ src/main/java/com/fish/Example.java
@@ -0,0 +1,11 @@
+package com.fish;
+
+import com.birdbrain.Finch;
+
+public class Example {
+    public static void main(String[] args) {
+        Finch fish = new Finch();
+
+        fish.setMove("Forward", 4.0, 4.0);
+    }
+}
