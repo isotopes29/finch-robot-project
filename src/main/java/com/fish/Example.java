38d7c8e45c6fef0e14a53e11567d39aaacc366ff
will <bcannon0001@mymail.lausd.net>
2025-06-02 14:46:14 -0700

    Update Example.java

--- src/main/java/com/fish/Example.java
+++ src/main/java/com/fish/Example.java
@@ -6,6 +6,17 @@ public class Example {
     public static void main(String[] args) {
         Finch fish = new Finch();
 
+        fish.setMove("Forward", 50.0, 50.0);
+        fish.setTurn("L", 180, 90);
         fish.setMove("Forward", 4.0, 4.0);
+        fish.playNote(60, 0.6);
     }
 }
+/**
+     * Set the specified tail led to the specified rgb color.
+     * @param ledNum - led to set (Range: 1 to 4)
+     * @param red - red intensity (Range: 0 to 100)
+     * @param green - green intensity (Range: 0 to 100)
+     * @param blue - blue intensity (Range: 0 to 100)
+     */
+    //public void setTail(int ledNum, int red, int green, int blue) {
