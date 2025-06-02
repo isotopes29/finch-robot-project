fb489dc69487ad38551dfbbfc66d7c74823c0b34
will <bcannon0001@mymail.lausd.net>
2025-06-02 15:15:09 -0700

    e

--- src/main/java/com/fish/Example.java
+++ src/main/java/com/fish/Example.java
@@ -8,14 +8,14 @@ public class Example {
 
         boolean workshop = true;
         while (workshop){
-            turbo.setTail("all", 0, 255, 255);
+            //turbo.setTail("all", 0, 255, 255);
             turbo.setBeak(231, 115, 44);
             
             turbo.setMove("Forward", 4.0, 50.0);
             turbo.setTurn("L", 180, 90);
             turbo.setMove("Forward", 4.0, 50.0);
     
-            turbo.setTail("all", 0, 0, 0);
+            //turbo.setTail("all", 0, 0, 0);
             turbo.setBeak(0, 0, 0);
 
         }
