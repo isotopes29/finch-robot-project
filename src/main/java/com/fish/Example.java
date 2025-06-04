3648dd35edba100d734847ede41c6ccd12acfebf
will <bcannon0001@mymail.lausd.net>
2025-06-04 10:20:00 -0700

    Update Example.java

--- src/main/java/com/fish/Example.java
+++ src/main/java/com/fish/Example.java
@@ -6,24 +6,15 @@ public class Example {
     public static void main(String[] args) {
         Finch turbo = new Finch();
 
-        /**boolean workshop = true;
-        for (int i = 0; i < 5; i++){
-            //turbo.setTail("all", 0, 255, 255);
-            turbo.setBeak(231, 115, 44);
-            
-            turbo.setMove("Forward", 4.0, 50.0);
-            turbo.setTurn("L", 180, 90);
-            turbo.setMove("Forward", 4.0, 50.0);
-    
-            //turbo.setTail("all", 0, 0, 0);
-            turbo.setBeak(0, 0, 0);
-            turbo.pause(0.3);
-        }*/
-
         // ITS TIME TO GET FUNKY
         
         
-        // TO THE LEFT
+        /** 
+         * TO THE LEFT
+         * finch bird's beak will light up every time it is in motion
+         * moves to the left by 90 degrees and then forward to correspond with the song lyric
+         * then will return to the normal standing by going right by 90 degrees
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("L", 90, 100);
         turbo.setMove("Forward", 2, 100);
@@ -32,7 +23,10 @@ public class Example {
         turbo.setTurn("R", 90, 100);
         turbo.pause(.01);
         
-        //TAKE IT BACK NOW YALL
+        /** 
+         * TAKE IT BACK NOW YALL
+         * moves backwards by 1, pauses briefly, then backwards again
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setMove("Backward", 1, 100);
         turbo.setBeak(0, 0, 0);
@@ -42,20 +36,30 @@ public class Example {
         turbo.setBeak(0, 0, 0);
         turbo.pause(.3);
         
-        // ONE HOP THIS TIME
+        /** 
+         * ONE HOP THIS TIME
+         * finch birds cannot hop
+         * so i made turbo spin to the left instead
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("L", 360, 100);
         turbo.setBeak(0, 0, 0);
         turbo.pause(.1);
 
-        //right foot LETS STOMP
+        /** 
+         * RIGHT FOOT LETS STOMP
+         * again these birds have no legs. so he just looks to the right very briefly before turning to the left
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("R", 45, 100);
         turbo.setBeak(0, 0, 0);
         turbo.setTurn("L", 45, 100);
         turbo.pause(.01);
 
-        // ELFT FOOT LETS STOMP
+        /** 
+         *  ELFT FOOT LETS STOMP
+         * same thing as the right but. to the left. who wouldve guessed.
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("L", 45, 100);
         turbo.setBeak(0, 0, 0);
@@ -63,7 +67,10 @@ public class Example {
         turbo.pause(.1);
 
         
-        // cha cha real smooth
+        /** 
+         * cha cha real smooth
+         * bird moves back and forth in rapid succession
+         * */
         turbo.setBeak(231, 115, 44);
         turbo.setMove("Forward", 2, 100);
         turbo.setMove("Backward", 2, 100);
@@ -74,7 +81,7 @@ public class Example {
         turbo.setBeak(0, 0, 0);
         turbo.pause(.9);
 
-        // TO THE LEFT
+        //TO THE LEFT
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("L", 90, 100);
         turbo.setMove("Forward", 2, 100);
@@ -126,17 +133,25 @@ public class Example {
         //NOW ITS TIME TO GET FUNKYYYYY
 
         // TO THE RIGHT NOW
+        /**
+         * briefly turns to the right and blinks
+         */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("R", 30, 100);
         turbo.setBeak(0, 0, 0);
 
-        // TO THE LEFT
+        /**
+         * to the left
+         * turns extra left to compensate the right it just went
+         * then return to normal direction
+         */
         turbo.setBeak(231, 115, 44);
         turbo.setTurn("L", 120, 100);
         turbo.setBeak(0, 0, 0);
         turbo.setTurn("R", 90, 100);
 
-        //TAKE IT BACK NOW YALL
+        //its literally just the same stuff that can't be looped because the timing is different
+        // bear with me
         turbo.setBeak(231, 115, 44);
         turbo.setMove("Backward", 1, 100);
         turbo.setMove("Backward", 1, 100);
@@ -179,7 +194,11 @@ public class Example {
         turbo.setBeak(0, 0, 0);
         turbo.pause(.5);
 
-        //CRISS CROSS
+        /**
+         * see this one is actually new
+         * goes backwards twice
+         * small pause
+         */
         turbo.setBeak(231, 115, 44);
         turbo.setMove("B", 1, 100);
         turbo.setMove("B", 1, 100);
